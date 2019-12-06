01c339555991bafa7110fca2ff62e4894e052643
Terentev Kirill <mowder96@gmail.com>
2019-12-06 20:23:25 +0500

    Static container

---
+++ Assets/Scripts/StaticContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StaticContainer
+{
+	private static Dictionary<Type, object> _container = new Dictionary<Type, object>();
+
+	public static void Add(Type type, object obj)
+	{
+		if (_container.ContainsKey(type))
+		{
+			_container[type] = obj;
+			return;
+		}
+
+		_container.Add(type, obj);	
+	}
+
+	public static object Get(Type type)
+	{
+		return _container.ContainsKey(type) ? _container[type] : null;
+	}
+
+	public static void Remove(Type type)
+	{
+		if (!_container.ContainsKey(type))
+		{
+			return;
+		}
+
+		_container.Remove(type);
+	}
+}
