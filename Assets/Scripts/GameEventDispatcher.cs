8469bb238849128aed5cac8478ea67eeee6ffe98
Terentev Kirill <mowder96@gmail.com>
2019-12-06 21:39:15 +0500

    GameEventDispatcher

---
+++ Assets/Scripts/GameEventDispatcher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine.Events;
+using Debug = UnityEngine.Debug;
+
+public class MyEvent : UnityEvent<object[]> { }
+
+public class GameEventDispatcher :  EventDispatcher
+{
+	private Dictionary<GameEvents, MyEvent> _events;
+
+	public void AddListener(GameEvents name, UnityAction<object[]> action)
+	{
+		if (_events.ContainsKey(name))
+		{
+			_events[name].AddListener(action);
+			return;
+		}
+
+		_events.Add(name, new MyEvent());
+		_events[name].AddListener(action);
+	}
+
+	public void RemoveListener(GameEvents name, UnityAction<object[]> action)
+	{
+		if (!_events.ContainsKey(name))
+		{
+			return;
+		}
+
+		_events[name].RemoveListener(action);
+
+		if (_events[name].GetPersistentEventCount() < 1)
+		{
+			_events.Remove(name);
+		}
+	}
+
+	public void RemoveAllListeners(GameEvents name)
+	{
+		if (!_events.ContainsKey(name))
+		{
+			return;
+		}
+
+		_events[name].RemoveAllListeners();
+		_events.Remove(name);
+	}
+
+	public void InvokeEvent(GameEvents name, params object[] args)
+	{
+		if (!_events.ContainsKey(name))
+		{
+			Debug.LogError($"Event with id({name}) does not contain in EventList!");
+			return;
+		}
+
+		_events[name].Invoke(args);
+	}
+}
--- Assets/Scripts/Interfaces/EventDispatcher.cs
+++ Assets/Scripts/Interfaces/EventDispatcher.cs
@@ -4,8 +4,8 @@ using UnityEngine.Events;
 
 public interface EventDispatcher
 {
-	void AddListener(GameEvents name, UnityEvent action);
-	void RemoveListener(GameEvents name, UnityEvent action);
+	void AddListener(GameEvents name, UnityAction<object[]> action);
+	void RemoveListener(GameEvents name, UnityAction<object[]> action);
 	void RemoveAllListeners(GameEvents name);
-	void InvokeEvent(GameEvents name);
+	void InvokeEvent(GameEvents name, params object[] args);
 }
